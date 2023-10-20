a797c70f3beddea4a58f5e25f249348381495539
JoileJr <joilejunior2@gmail.com>
2023-10-20 20:00:10 -0300

    a

---
+++ src/main/java/com/aula/backend/service/ProdutoService.java
@@ -0,0 +1,34 @@
+package com.aula.backend.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.aula.backend.entity.Produto;
+import com.aula.backend.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+    
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Page<Produto> listarTodos(Pageable pageable){
+        return produtoRepository.findAll(pageable);
+    }
+
+    public Produto salvar(Produto produto){
+        return produtoRepository.saveAndFlush(produto);
+    }
+
+    public Produto atualizar(Produto produto) {
+        return produtoRepository.saveAndFlush(produto);
+    }
+
+    public void excluir(Long id){
+        produtoRepository.deleteById(id);
+    }
+
+}
